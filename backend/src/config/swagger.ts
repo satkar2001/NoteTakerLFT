74cd4405360dbc7399d265aee991169d7d6fb1c6
Satkar <satkar.acharya@yco.com.np>
2025-08-15 00:58:10 +0545

    swagger update 2

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -2,8 +2,10 @@ import swaggerJsdoc from 'swagger-jsdoc';
 
 const options = {
   definition: {
+    openapi: '3.0.0',
     info: {
       title: 'NoteTaker API',
+      version: '1.0.0',
       description: 'Notes App for LFT by Satkar',
 
     },
