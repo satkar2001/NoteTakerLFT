4e8de041ca7e51b139aff797756c470cf97a3de7
Satkar <satkar.acharya@yco.com.np>
2025-08-14 23:17:19 +0545

    name change

--- backend/src/utils/emailService.ts
+++ backend/src/utils/emailService.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 // Create a transporter using Gmail SMTP
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER, // Your Gmail address
